67c1fef65129e0be6ee98ca693399dcae2b7ec6a
mnimmz <nimmons92@gmail.com>
2017-07-04 17:46:03 -0400

    Day 4 homework

---
+++ Day_4/src/projects/Multiplicationof9.java
@@ -0,0 +1,28 @@
+package projects;
+
+
+public class Multiplicationof9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Nine Multiplication Table");
+		System.out.println(" ");
+		
+		System.out.println(1 * 9);
+		System.out.println(2 * 9);
+		System.out.println(3 * 9);
+		System.out.println(4 * 9);
+		System.out.println(5 * 9);
+		System.out.println(6 * 9);
+		System.out.println(7 * 9);
+		System.out.println(8 * 9);
+		System.out.println(9 * 9);
+		System.out.println(10 * 9);
+		System.out.println(11 * 9);
+		System.out.println(12 * 9);
+		
+		
+	}
+
+}
